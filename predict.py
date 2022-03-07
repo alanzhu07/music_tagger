cf66adec48f8a2e2340c61934a778187ce12ab0f
alanzhu07 <alanzhuyixuan@gmail.com>
2022-03-07 14:49:01 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 from training import model
 # import model
 
-SAMPLE_RATE = 16000
+# SAMPLE_RATE = 16000
+SAMPLE_RATE = 44100
 DATASET = "mtat"
 MODEL_NAMES = {
     "Self-attention": "attention",
@@ -92,7 +93,38 @@ class Predictor():
         print("x.max(), x.min(), x.mean()", x.max(), x.min(), x.mean())
         # asdf()
         out = model(x)
-        result = dict(zip(self.tags, out[0].detach().numpy().tolist()))
+        result = dict(zip(self.tags, out[0].detach().cpu().numpy().tolist()))
+
+        if output_format == "JSON":
+            return result
+
+        result_list = list(sorted(result.items(), key=lambda x: x[1]))
+        plt.figure(figsize=[5, 10])
+        plt.barh(
+            np.arange(len(result_list)), [r[1] for r in result_list], align="center"
+        )
+        plt.yticks(np.arange(len(result_list)), [r[0] for r in result_list])
+        plt.tight_layout()
+
+        out_path = Path(tempfile.mkdtemp()) / "out.png"
+        plt.savefig(out_path)
+        return 
+
+    def predict_from_signal(self, signal, variant, output_format):
+        key = MODEL_NAMES[variant]
+        model = self.models[key].eval()
+        input_length = self.input_lengths[key]
+        # signal, _ = librosa.core.load(str(input), sr=SAMPLE_RATE)
+        length = len(signal)
+        hop = length // 2 - input_length // 2
+        print("length, input_length", length, input_length)
+        x = torch.zeros(1, input_length)
+        x[0] = torch.Tensor(signal[hop : hop + input_length]).unsqueeze(0)
+        x = Variable(x.to(self.device))
+        print("x.max(), x.min(), x.mean()", x.max(), x.min(), x.mean())
+        # asdf()
+        out = model(x)
+        result = dict(zip(self.tags, out[0].detach().cpu().numpy().tolist()))
 
         if output_format == "JSON":
             return result
